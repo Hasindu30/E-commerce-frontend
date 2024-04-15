ac1f1fe02f9d34471da1cfcbbbebfc29f120d250
Hasindu30 <wohchandeepa@std.appsc.sab.ac.lk>
2024-04-16 01:16:56 +0530

    modified:   public/index.html 	modified:   src/Components/Navbar/Narbar.css 	modified:   src/Components/Navbar/Navbar.jsx 	new file:   src/Pages/Shop.css new file:   src/Pages/Shop.jsx 	modified:   src/index.css

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,20 +1,23 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './Narbar.css'
 import logo from '../Assets/logo.png'
 import cart from '../Assets/cart.png'
 
 
+
+
 const Navbar = () => {
+  const [menu,setMenu] = useState("shop");
   return (
     <div className='navbar'>
       <div className='logo'>
         <img src={logo} alt="" />
       </div>
       <ul className='nav-menu'>
-        <li>Shop<hr/></li>
-        <li>Men</li>
-        <li>Women</li>
-        <li>Kids</li>
+        <li onClick={()=>{setMenu("shop")}}>Shop {menu==="shop"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("mens")}}>Men {menu==="mens"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("womens")}}>Women {menu==="womens"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("kids")}}>Kids {menu==="kids"?<hr/>:<></>}</li>
       </ul>
       <div className="nav-login-cart">
         <button>Login</button>
