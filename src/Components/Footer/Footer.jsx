8e6d77795d49f4a734d70521e56720da77ad363c
Hasindu30 <wohchandeepa@std.appsc.sab.ac.lk>
2024-04-17 13:26:39 +0530

    modified:   src/App.js 	new file:   src/Components/Assets/instagram_icon.png 	new file:   src/Components/Assets/pintester_icon.png 	new file:   src/Components/Assets/whatsapp_icon.png 	modified:   src/Components/Footer/Footer.css 	modified:   src/Components/Footer/Footer.jsx

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
 import './Footer.css'
-
+import flogo from '../Assets/logo.png'
+import instagram from '../Assets/instagram_icon.png'
+import pintester from '../Assets/pintester_icon.png'
+import whatsapp from'../Assets/whatsapp_icon.png'
 const Footer = () => {
   return (
     <div className='footer'>
+      <div className="footer-logo">
+        <img src={flogo} alt="" />
+        
+      </div>
+      <ul className="footer-links">
+        <li>Comapny</li>
+        <li>Products</li>
+        <li>Offices</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+      <div className="footer-social-icon">
+        <div className="footer-icons-container">
+            <img src={instagram} alt="" />
+        </div>
+        <div className="footer-icons-container">
+            <img src={pintester} alt="" />
+        </div>
+        <div className="footer-icons-container">
+            <img src={whatsapp} alt="" />
+        </div>
+        </div>
+        <div className="footer-copyright">
+           <hr/>
+           <p>Copyright @2024 - All Right Reserved</p>
+        </div>
       
     </div>
   )
