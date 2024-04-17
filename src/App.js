8eaf6f8f63fd22b333e64cfa7f9dbec213f53e57
Hasindu30 <wohchandeepa@std.appsc.sab.ac.lk>
2024-04-17 14:36:05 +0530

    footer update

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import Cart from './Pages/Cart';
 import Product from './Pages/Product';
 import LoginSignup from './Pages/LoginSignup';
 import Footer from './Components/Footer/Footer';
+import men_banner from './Components/Assets/banner_mens.png';
+import women_banner from './Components/Assets/banner_women.png';
+import kids_banner from './Components/Assets/banner_kids.png';
 
 function App() {
   return (
@@ -15,9 +18,9 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path='/' element={<Shop/>}/>
-        <Route path='/mens' element={<ShopCategory category="mens"/>}/>
-        <Route path='/womens' element={<ShopCategory category="womens"/>}/>
-        <Route path='/Kids' element={<ShopCategory category="kid"/>}/>
+        <Route path='/mens' element={<ShopCategory banner={men_banner} category="mens"/>}/>
+        <Route path='/womens' element={<ShopCategory banner={women_banner} category="womens"/>}/>
+        <Route path='/Kids' element={<ShopCategory banner={kids_banner} category="kid"/>}/>
         <Route path='product' element={<Product/>}></Route>
           <Route path=':productId' element={<Product/>}></Route>
         <Route path='/cart' element={<Cart/>}></Route>
