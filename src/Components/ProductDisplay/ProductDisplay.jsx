6564bb4aae276d872ac5f00d115b892aeb5c8f69
Hasindu30 <wohchandeepa@std.appsc.sab.ac.lk>
2024-04-18 09:51:46 +0530

    renamed:    src/Components/Breadcrums/Breadcrums.jsx -> src/Components/Breadcrums/Breadcrum.jsx 	modified:   src/Components/ProductDisplay/ProductDisplay.jsx 	modified:   src/Pages/Product.jsx

--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -1,24 +1,22 @@
 import React from 'react'
 import './ProductDisplay.css'
-import start_dull_icon from "../Assets/star_dull_icon.png"
-import start_icon from "../Assets/star_icon.png"
+import start_dull_icon from "../Assets/star_dull_icon.png";
+import start_icon from "../Assets/star_icon.png";
 
 const ProductDisplay = (props) => {
     const {product} =props;
   return (
     <div className='productdisplay'>
       <div className="productdisplay-left">
-        
-            <div className="productdisplay-list">
+            <div className="productdisplay-img-list">
                 <img src={product.image} alt="" />
                 <img src={product.image} alt="" />
                 <img src={product.image} alt="" />
                 <img src={product.image} alt="" />
             </div>
-            
-        </div>
-        <div className="productdisplay-img">
-            <img className='productdisplay-main-img' alt={product.image} />
+            <div className="productdisplay-img">
+            <img className='productdisplay-main-img' src={product.image} alt='' />
+            </div>
         </div>
       <div className="productdisplay-right">
         <h1>{product.name}</h1>
