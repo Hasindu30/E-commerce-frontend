c2a8853a67e3e4be6800966e9fa2d8b1e419e0ec
Hasindu30 <wohchandeepa@std.appsc.sab.ac.lk>
2024-04-19 15:16:40 +0530

    modified:   src/Pages/Product.jsx

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,16 +1,16 @@
 import React,{useContext} from 'react'
 import { ShopContext } from '../Context/Context';
 import { useParams } from 'react-router-dom';
-import Breadcrums from '../Components/Breadcrums/Breadcrum';
-import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
+import Breadcrum from '../Components/Breadcrums/Breadcrum';
+
 const Product = () => {
   const {all_product}=useContext(ShopContext);
-  const {productId} =useParams();
+  const {productId} = useParams();
   const product =all_product.find((e)=>e.id === Number(productId));
   return (
     <div>
-      <Breadcrums product={product}/>
-      <ProductDisplay product={product}/>
+      <Breadcrum product={product}/>
+      
     </div>
   )
 }
